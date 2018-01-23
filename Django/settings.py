cfa7de481f8a01f7606d854213dcc8cac0f2b60f
Katolik4 <piotr.polakowski92@gmail.com>
2018-01-23 23:06:12 +0100

    V1.0

--- Django/settings.py
+++ Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pq&1%7ok3qq7tovvz0l9cq8bb$l2z2v1!_-ll&r%%tw^m35*+='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.14', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.12', 'localhost', '127.0.0.1']
 
 
 # Application definition
